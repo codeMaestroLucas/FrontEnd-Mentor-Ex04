8017407f493e8301104b7b657d6b1828fd9a1d65
Lucas Samuel <143713992+codeMaestroLucas@users.noreply.github.com>
2023-10-10 20:56:26 -0300

    First upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# Frontend Mentor - Newsletter sign-up form with success message
+## Table of Contents
+
+- [Screenshots](#screenshots)
+- [Author](#author)
+
+## Screenshots
+
+- Project 375px:
+    1. [Mobile-preview](./prints/Mobile.png)
+    2. [Mobile-denied](./prints/Mobile-deneid.png)
+    3. [Mobile-confirm](./prints/Mobile-confirm.png)
+
+- Project 1440px:
+    1. [Desktop-preview](./prints/Desktop.png)
+    2. [Desktop-actives](./prints/Desktop-actives.png)
+    3. [Desktop-denied](./prints/Desktop-denied.png)
+    4. [Desktop-confirm](./prints/Desktop-confirm.png)
+
+
+## Author
+
+- LinkedIn - [Lucas Samuel Lemos Rajão](https://www.linkedin.com/in/lucas-samuel-64846a253/)
+- Frontend Mentor - [@codeMaestroLucas](https://github.com/codeMaestroLucas)
+
+Im studing IT and discovering this new wolrd in baby steps.
---
+++ src/js/index.js
@@ -0,0 +1,53 @@
+const card = document.querySelectorAll('.card')[0];
+const sucess = document.querySelectorAll('.sucess')[0];
+
+const input = document.getElementById('email');
+const btn = document.getElementById('btn');
+const txtValidation = document.querySelectorAll('.validation')[0];
+
+const btnConfirm = document.getElementById('btnConfirm');
+
+function validate() {
+    if (input.value.trim() === '' || !input.value.includes('@')) {
+        input.classList.add('denied');
+        txtValidation.classList.remove('hide');
+        return false; // Retorna falso para indicar que a validação falhou
+    } else {
+        input.classList.remove('denied');
+        txtValidation.classList.add('hide');
+        return true; // Retorna true para indicar que a validação passou
+    }
+}
+
+function showDisplay() {
+    if (!card.classList.contains('hide')) {
+        card.classList.add('hide');
+        sucess.classList.remove('hide');
+    }
+}
+
+function hideSucess() {
+    if (!sucess.classList.contains('hide')) {
+        sucess.classList.add('hide');
+    }
+}
+
+function emailSuccess () {
+    const typedEmail = input.value;
+    const span = document.getElementById('typedEmail');
+    span.innerHTML = typedEmail;
+}
+
+btn.addEventListener('click', function(event) {
+    check = validate();
+    if (! check) {
+        event.preventDefault(); // Impede o envio do formulário se a validação falhar
+    } else {
+        showDisplay();
+        emailSuccess();
+        event.preventDefault();
+
+    }
+});
+
+btnConfirm.addEventListener('click', hideSucess);
